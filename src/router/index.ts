60fbaf965d262bc22d4e724570ebe71cae60461b
Leonardo Amador <amadorleonardo8109@gmail.com>
2023-07-21 11:31:37 -0300

    Refactoring using DRY.

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,31 +4,7 @@ import Home from "../views/Home.vue";
 const routes: Array<RouteRecordRaw> = [
   { path: "/", name: "home", component: Home },
   {
-    path: "/brazil",
-    name: "brazil",
-    component: () =>
-      import(/* WebpackChunkName: "brasil" */ "@/views/Brazil.vue"),
-  },
-  {
-    path: "/hawaii",
-    name: "hawaii",
-    component: () =>
-      import(/* WebpackChunkName: "hawaii" */ "@/views/Hawaii.vue"),
-  },
-  {
-    path: "/jamaica",
-    name: "jamaica",
-    component: () =>
-      import(/* WebpackChunkName: "jamaica" */ "@/views/Jamaica.vue"),
-  },
-  {
-    path: "/panama",
-    name: "panama",
-    component: () =>
-      import(/* WebpackChunkName: "panama" */ "@/views/Panama.vue"),
-  },
-  {
-    path: "/destination/:id",
+    path: "/destination/:id/:slug",
     name: "destination.show",
     component: () => import("@/views/DestinationShow.vue"),
   },
